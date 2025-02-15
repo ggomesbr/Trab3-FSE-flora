fe3cc25339502fa0888f25f0c029ffab6b593717
Gabriel Gomes <gabriel.luiz77888@gmail.com>
2025-02-15 17:15:47 -0300

    Adicionando implementacao do sensor de nivel de agua

--- main/src/main.c
+++ main/src/main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
-#include <math.h>
-#include "driver/adc.h"
-#include "dht11.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
 
 #include "ldr.h"
+#include "dht11.h"
+#include "water_sensor.h"
 
 #define TAG "MAIN"
 
-
-void app_main()
+void app_main(void)
 {
+    esp_log_level_set(TAG, ESP_LOG_INFO);
+    ESP_LOGI(TAG, "Iniciando o sistema de monitoramento...");
 
-    xTaskCreate(&ldr_task, "consulta_ldr", 2048, NULL, 3,NULL);
-    xTaskCreate(&dht11_task, "consulta_dht11", 2048, NULL, 3, NULL);
+    // Criando as tarefas para cada sensor
+    xTaskCreate(&ldr_task, "ldr_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&dht11_task, "dht11_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&water_sensor_task, "water_sensor_task", 2048, NULL, 5, NULL);
+
+    ESP_LOGI(TAG, "Todas as tarefas foram iniciadas");
 }
